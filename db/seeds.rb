375ba57472042b83f1dcd8d776e7f31f6dde7ca0
Sandhya <bs.sandhya.in@gmail.com>
2017-09-27 22:27:47 +0530

    category table change

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Category.create(name:'Nos Expression')
-Category.create(name:'En Social')
-Category.create(name:'A table')
-Category.create(name:'cultural')
